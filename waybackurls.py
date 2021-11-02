68d37cf6e28718242680711bc07b39bce211deeb
asish <asishsajeev@gmail.com>
2021-11-02 19:23:53 +0530

    alive urls only, removed duplicate urls

--- waybackurls.py
+++ waybackurls.py
@@ -12,16 +12,25 @@ parser.add_argument("-k", "--keywords", help = "Keywords to search in wayurls li
 # Read arguments from command line
 args = parser.parse_args()
 
-if args.keywords:
-    keywords = args.keywords.split(',')
-
 page = requests.get("https://web.archive.org/cdx/search/cdx?url="+args.url+"/*&output=text&fl=original&collapse=urlkey")
 
 content = page.content.decode('utf-8')
 
 waybackurls = content.splitlines()
 
+prev_url = ""
+
 for url in waybackurls:
-    for keyword in keywords:
-        if keyword in url:
-            print(url)
+    if url != prev_url:
+        current_page = requests.get(url)
+        if current_page.status_code == 200:
+            if args.keywords:
+                keywords = args.keywords.split(',')
+                for keyword in keywords:
+                    if keyword in url:
+                            print(url)
+                            break
+            else:
+                print(url)
+    
+    prev_url = url
